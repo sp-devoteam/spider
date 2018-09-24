b8674921a6139e0068c37389b7d03a30ceb51871
pstiebel <pstiebel@users.noreply.github.com>
2018-09-24 14:49:21 +0200

    Update dbconnect.php
    mysql methods changed from mysql_* to mysqli_*

--- dbconnect.php
+++ dbconnect.php
@@ -5,11 +5,11 @@ $db_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $db_user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $db_pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 global $db;
-$db = mysql_connect($db_host,$db_user,$db_pass);
+$db = mysqli_connect($db_host,$db_user,$db_pass);
 	if (!$db) {
 	die("Unable to connect to database");
 	}
-if (!mysql_select_db('spider')) {
+if (!mysqli_select_db('spider')) {
 		die("Unable to access spider database");
 	}
 }
