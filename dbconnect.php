8a2c90f23adf42c2e0e3f2cd713ec33d78121ef0
pstiebel <pstiebel@users.noreply.github.com>
2018-09-25 13:35:23 +0200

    Added Env variable for db name

--- dbconnect.php
+++ dbconnect.php
@@ -4,12 +4,15 @@ function connectDB() {
 $db_host = getenv('OPENSHIFT_MYSQL_DB_HOST'); 
 $db_user = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 $db_pass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+$db_name = getenv('OPENSHIFT_MYSQL_DB_NAME');
+	
 global $db;
 $db = mysqli_connect($db_host,$db_user,$db_pass);
 	if (!$db) {
 	die("Unable to connect to database");
 	}
-if (!mysqli_select_db('spider')) {
+##if (!mysqli_select_db('spider')) {
+if (!mysqli_select_db($db_name)) {
 		die("Unable to access spider database");
 	}
 }
