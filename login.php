4020578d749ee0a9b849cbb4561c7df0105c0632
sp-devoteam <43409545+sp-devoteam@users.noreply.github.com>
2018-10-04 10:02:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -64,7 +64,7 @@ connectDB();
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="assess.php"><img src="images/innovate.png">  Ready to Innovate?</a>
+			<a class="navbar-brand" href="index.php"><img src="images/innovate.png">  Ready to Innovate?</a>
 		</div>
 		<!-- menu items -->
 		<div class="collapse navbar-collapse" id="navbar1">
