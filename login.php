664db740d5d404884e97e1e85c9e52c1971d7d59
sp-devoteam <43409545+sp-devoteam@users.noreply.github.com>
2018-10-05 15:09:02 +0200

    Update login.php

--- login.php
+++ login.php
@@ -58,12 +58,6 @@ connectDB();
 	<div class="container-fluid">
 		<!-- add header -->
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
 			<a class="navbar-brand" href="index.php"><img src="images/innovate.png">  Ready to Innovate?</a>
 		</div>
 		<!-- menu items -->
