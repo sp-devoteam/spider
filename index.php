a526f8d4d9f245d595dccc08b49ed1ae3c7034e9
sp-devoteam <43409545+sp-devoteam@users.noreply.github.com>
2018-10-05 13:29:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -110,12 +110,6 @@ h3 { color: #7c795d; font-family: 'Source Sans Pro', sans-serif; font-size: 28px
 <nav class="navbar navbar-default" role="navigation">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
 			<a class="navbar-brand" href="index.php"><img src="images/innovate.png">  Ready to Innovate?</a>
 		</div>
 		<div class="collapse navbar-collapse" id="navbar1">
