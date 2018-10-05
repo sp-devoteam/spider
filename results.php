d41e3c44d6ea6bc0dd003af4ecd8f006a0743886
sp-devoteam <43409545+sp-devoteam@users.noreply.github.com>
2018-10-05 10:53:43 +0200

    Update results.php

--- results.php
+++ results.php
@@ -292,6 +292,30 @@ table {
 <?php  date_default_timezone_set("Europe/London");
 #phpinfo();
  ?>
+	
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container-fluid">
+		<!-- add header -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="index.php"><img src="images/innovate.png">  Ready to Innovate?</a>
+		</div>
+		<!-- menu items -->
+		<div class="collapse navbar-collapse" id="navbar1">
+			<ul class="nav navbar-nav navbar-right">
+				<li class="active"><a href="login.php">Login</a></li>
+				<li><a href="register.php">Sign Up</a></li>
+				<li><a href="https://ready-to-innovate.com/blog/using-rti/using-ready-to-innovate-rti/">Blog</a></li>
+
+			</ul>
+		</div>
+	</div>
+</nav>
       <div id="wrapper">
 
       <header>
